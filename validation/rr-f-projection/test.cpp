2346843205fd0000124568b521abb04c524ef5ae
Frooglemcdoo <mattn8423@gmail.com>
2026-09-26 14:55:59 -0500

    GI26 validate affine camera matrix expansion

--- validation/rr-f-projection/test.cpp
+++ validation/rr-f-projection/test.cpp
@@ -10,4 +10,14 @@ int main(){
  p[0]=1e100;assert(!control_rr::CopyValidatedProjection(p,q,out));
  double zero[16]{};assert(!control_rr::CopyValidatedProjection(zero,zero,out));
  assert(!control_rr::CopyValidatedProjection(nullptr,q,out));
+ double w2v[12]={1,0,0,0,1,0,0,0,1,-10,-20,-30};
+ double v2w[12]={1,0,0,0,1,0,0,0,1,10,20,30};
+ float affine[16]{};
+ assert(control_rr::CopyValidatedAffine43(w2v,v2w,affine));
+ assert(affine[0]==1&&affine[5]==1&&affine[10]==1&&affine[15]==1);
+ assert(affine[12]==-10&&affine[13]==-20&&affine[14]==-30);
+ v2w[9]=11;affine[12]=77;
+ assert(!control_rr::CopyValidatedAffine43(w2v,v2w,affine)&&affine[12]==77);
+ v2w[9]=10;w2v[0]=std::numeric_limits<double>::quiet_NaN();
+ assert(!control_rr::CopyValidatedAffine43(w2v,v2w,affine));
 }
